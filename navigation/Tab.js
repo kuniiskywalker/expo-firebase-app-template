7222d75a63bcca46684841eb905f0012ad158bca
國井弘一 <kuniiskywalker@gmail.com>
2019-03-19 16:10:20 +0900

    add scroll tabs

--- navigation/Tab.js
+++ navigation/Tab.js
@@ -1,5 +1,49 @@
-import { createBottomTabNavigator } from "react-navigation-tabs";
-import Home from "../screens/HomeScreen";
+import React from 'react';
+import { createMaterialTopTabNavigator, createBottomTabNavigator } from "react-navigation-tabs";
+import Tab from "../screens/HomeScreen";
+
+const Home = createMaterialTopTabNavigator(
+    {
+        Tab1: {
+            screen: props => <Tab {...props} kind={"Tab1"} />,
+            navigationOptions: ({navigation}) => ({
+                title: 'tab1',
+            }),
+        },
+        Tab2: {
+            screen: props => <Tab {...props} kind={"Tab2"} />,
+            navigationOptions: ({navigation}) => ({
+                title: 'tab2',
+            }),
+        },
+        Tab3: {
+            screen: props => <Tab {...props} kind={"Tab3"} />,
+            navigationOptions: ({navigation}) => ({
+                title: 'Tab3',
+            }),
+        }
+    },
+    {
+        initialRouteName: 'Tab1',
+        headerMode: 'none',
+        tabBarOptions: {
+            scrollEnabled: true,
+            labelStyle: {
+                fontSize: 12,
+                color: 'black',
+            },
+            tabStyle: {
+                width: 130,
+            },
+            style: {
+                backgroundColor: 'white',
+            },
+            indicatorStyle: {
+                backgroundColor: '#ff0000',
+            },
+        }
+    }
+);
 
 export default createBottomTabNavigator({
     TabItem1: {
